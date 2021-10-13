59d06072f119ca5b5f90aad5a81bc2d736be8366
Rick <ricksmycodeguy@gmail.com>
2021-10-13 14:07:19 -0500

    test 1-18 passing

--- index.js
+++ index.js
@@ -118,7 +118,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -135,7 +136,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const milesAbleToDrive = this.tank * this.mpg
+    if (distance <= milesAbleToDrive){
+      this.odometer = this.odometer + distance 
+      this.tank = this.tank - (distance / this.mpg)
+    }else {
+      this.odometer = this.odometer + milesAbleToDrive
+      this.tank = 0
+    }
+    return this.odometer
   }
 
   /**
@@ -150,7 +159,12 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    if(gallons <= this.tankSize - this.tank){
+      this.tank = this.tank + gallons
+    }else{
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
